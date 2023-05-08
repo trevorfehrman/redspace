d3a939d1fdd103d19db53b08d91bff9493ee624a
Trevor Fehrman <trevorfehrman@gmail.com>
2023-05-07 21:33:27 -0700

    removed export statement??????

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -2,7 +2,11 @@ import NextAuth from 'next-auth';
 import type { NextAuthOptions } from 'next-auth';
 import GithubProvider from 'next-auth/providers/github';
 
-export const authOptions: NextAuthOptions = {
+const authOptions: NextAuthOptions = {
+  session: {
+    strategy: 'jwt',
+  },
+
   providers: [
     GithubProvider({
       clientId: process.env.GITHUB_ID!,
