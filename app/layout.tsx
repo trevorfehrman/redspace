7cb3d332ee28c5a8dbfe2e9feb4a4c998059bb5c
Trevor Fehrman <trevorfehrman@gmail.com>
2023-05-08 16:34:11 -0700

    trying clerk again with google

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,6 @@
-// app/layout.tsx
-import AuthProvider from '@/components/AuthProvider';
 import './globals.css';
 import { Inter } from 'next/font/google';
+import { ClerkProvider } from '@clerk/nextjs';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -12,10 +11,10 @@ export const metadata = {
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
-    <AuthProvider>
+    <ClerkProvider>
       <html lang='en'>
         <body className={inter.className}>{children}</body>
       </html>
-    </AuthProvider>
+    </ClerkProvider>
   );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
+import { UserButton } from '@clerk/nextjs';
 import Image from 'next/image';
 
 export default function Home() {
   return (
     <main>
       <h1>sup</h1>
+      <UserButton />
     </main>
   );
 }
