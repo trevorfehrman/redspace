c44cea630f85ba7b3cb26810622c10a87152ac69
Trevor Fehrman <trevorfehrman@gmail.com>
2023-05-08 22:13:05 -0700

    unbreaking main page

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,11 @@
-import { UserButton } from '@clerk/nextjs';
+import { SignIn, UserButton } from '@clerk/nextjs';
 
 export default function Home() {
   return (
     <main>
       <h1>sup</h1>
       <UserButton />
+      <SignIn />
     </main>
   );
 }
